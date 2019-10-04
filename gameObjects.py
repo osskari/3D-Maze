8cb9d4d5f185b7b95d23d3997c5dd09fe1a4cf3d
osskari <oskarjorgen@gmail.com>
2019-10-04 14:29:20 +0000

    refactoring

--- gameObjects.py
+++ gameObjects.py
@@ -1,85 +1,149 @@
-# from math import *
-# # from baseObjects import Point, Vector
-#
-#
-# class Game:
-#     def __init__(self, shader, model_matrix, view_matrix, projection_matrix, player):
-#         self.shader = shader
-#         shader.use()
-#         self.model_matrix = model_matrix
-#         self.view_matrix = view_matrix
-#         self.player = player
-#         self.look()
-#         self.projection_matrix = projection_matrix
-#
-#     def look(self):
-#         self.view_matrix.look(self.player.position, self.player.looking_at, self.player.normal)
-#
-#     def set_perspective(self, fov, aspect, near, far):
-#         self.projection_matrix.set_perspective(fov, aspect, near, far)
-#         self.shader.set_projection_matrix(self.projection_matrix.get_matrix())
-#
-#
-# class Player:
-#     def __init__(self, position, speed, rotation_speed, looking_at):
-#         self.position = position
-#         self.speed = speed
-#         self.rotationSpeed = rotation_speed
-#         self.looking_at = Point(looking_at.x, self.position.y, looking_at.z)
-#         self.normal = Vector(0, 1, 0)
-#
-#
-# class Drawable:
-#     def __init__(self, diffuse, specular, position, scale, shininess):
-#         self.diffuse = diffuse
-#         self.specular = specular
-#         self.position = position
-#         self.scale = scale
-#         self.shininess = shininess
-#
-#
-# class Rectangle(Drawable):
-#     def __init__(self, color, position, scale):
-#         Drawable.__init__(self, color, position, scale)
-#
-#     def draw(self, model_matrix, shader, cube):
-#         # shader.set_solid_color(*self.color)
-#         model_matrix.add_translation(*self.position)
-#         # model_matrix.push_matrix()
-#         model_matrix.add_scale(*self.scale)
-#         shader.set_model_matrix(model_matrix.matrix)
-#         cube.draw()
-#         # model_matrix.pop_matrix()
-#
-#
-# class Maze:
-#     def __init__(self):
-#         self.walls = []
-#         self.lights = []
-#
-#
-# class Level:
-#     def __init__(self, floor, maze):
-#         self.floor = floor
-#         self.maze = maze
-#
-#     def collision(self, new_pos, offset, matrix):
-#         for wall in self.maze.walls:
-#             if matrix.is_between(new_pos, wall, offset):
-#                 return wall
-#         return None
-#
-#
-# class Light:
-#     def __init__(self, position, diffuse, spectral=None):
-#         self.position = position
-#         self.diffuse = diffuse
-#         self.spectral = spectral if spectral else diffuse
-#
-#
-# inputs = {
-#             "W": False,  # Walk forward
-#             "A": False,  # Turn left
-#             "S": False,  # Walk backwards
-#             "D": False   # Turn right
-#         }
+from matrices import *
+from shaders import *
+
+
+class Cube(Drawable):
+    def __init__(self, diffuse, specular, position, scale, shininess):
+        Drawable.__init__(self, diffuse, specular, position, scale, shininess)
+        self.position_array = [-0.5, -0.5, -0.5,
+                               -0.5, 0.5, -0.5,
+                               0.5, 0.5, -0.5,
+                               0.5, -0.5, -0.5,
+                               -0.5, -0.5, 0.5,
+                               -0.5, 0.5, 0.5,
+                               0.5, 0.5, 0.5,
+                               0.5, -0.5, 0.5,
+                               -0.5, -0.5, -0.5,
+                               0.5, -0.5, -0.5,
+                               0.5, -0.5, 0.5,
+                               -0.5, -0.5, 0.5,
+                               -0.5, 0.5, -0.5,
+                               0.5, 0.5, -0.5,
+                               0.5, 0.5, 0.5,
+                               -0.5, 0.5, 0.5,
+                               -0.5, -0.5, -0.5,
+                               -0.5, -0.5, 0.5,
+                               -0.5, 0.5, 0.5,
+                               -0.5, 0.5, -0.5,
+                               0.5, -0.5, -0.5,
+                               0.5, -0.5, 0.5,
+                               0.5, 0.5, 0.5,
+                               0.5, 0.5, -0.5]
+        self.normal_array = [0.0, 0.0, -1.0,
+                             0.0, 0.0, -1.0,
+                             0.0, 0.0, -1.0,
+                             0.0, 0.0, -1.0,
+                             0.0, 0.0, 1.0,
+                             0.0, 0.0, 1.0,
+                             0.0, 0.0, 1.0,
+                             0.0, 0.0, 1.0,
+                             0.0, -1.0, 0.0,
+                             0.0, -1.0, 0.0,
+                             0.0, -1.0, 0.0,
+                             0.0, -1.0, 0.0,
+                             0.0, 1.0, 0.0,
+                             0.0, 1.0, 0.0,
+                             0.0, 1.0, 0.0,
+                             0.0, 1.0, 0.0,
+                             -1.0, 0.0, 0.0,
+                             -1.0, 0.0, 0.0,
+                             -1.0, 0.0, 0.0,
+                             -1.0, 0.0, 0.0,
+                             1.0, 0.0, 0.0,
+                             1.0, 0.0, 0.0,
+                             1.0, 0.0, 0.0,
+                             1.0, 0.0, 0.0]
+
+    def draw(self, model_matrix, shader):
+        super(Cube, self).draw(model_matrix, shader)
+        # draw
+        for i in range(0, 21, 4):
+            glDrawArrays(GL_TRIANGLE_FAN, i, 4)
+
+
+class Sphere(Drawable):
+    def __init__(self, diffuse, specular, position, scale, shininess, stacks=12, slices=24):
+        Drawable.__init__(self, diffuse, specular, position, scale, shininess)
+        self.vertex_array = []
+        self.slices = slices
+
+        stack_interval = pi/stacks
+        slice_interval = 2.0*pi/slices
+        self.vertex_count = 0
+
+        for stack_count in range(stacks):
+            stack_angle = stack_count * stack_interval
+            for slice_count in range(slices + 1):
+                slice_angle = slice_count * slice_interval
+                self.vertex_array.append(sin(stack_angle) * cos(slice_angle))
+                self.vertex_array.append(cos(stack_angle))
+                self.vertex_array.append(sin(stack_angle) * sin(slice_angle))
+
+                self.vertex_array.append(sin(stack_angle + stack_interval) * cos(slice_angle))
+                self.vertex_array.append(cos(stack_angle + stack_interval))
+                self.vertex_array.append(sin(stack_angle + stack_interval) * sin(slice_angle))
+
+                self.vertex_count += 2
+        self.position_array = self.vertex_array
+        self.normal_array = self.vertex_array
+
+    def draw(self, model_matrix, shader):
+        super(Sphere, self).draw(model_matrix, shader)
+        # draw
+        for i in range(0, self.vertex_count, (self.slices + 1) * 2):
+            glDrawArrays(GL_TRIANGLE_STRIP, i, (self.slices + 1) * 2)
+
+
+class Game:
+    def __init__(self, player):
+        self.shader = Shader3D()
+        self.shader.use()
+        self.model_matrix = ModelMatrix()
+        self.view_matrix = ViewMatrix()
+        self.player = player
+        self.look()
+        self.projection_matrix = ProjectionMatrix()
+        self.maze = Maze()
+
+    def look(self):
+        self.view_matrix.look(self.player.position, self.player.looking_at, self.player.normal)
+
+    def set_perspective(self, fov, aspect, near, far):
+        self.projection_matrix.set_perspective(fov, aspect, near, far)
+        self.shader.set_projection_matrix(self.projection_matrix.get_matrix())
+
+
+class Player:
+    def __init__(self, position, speed, rotation_speed, looking_at):
+        self.position = position
+        self.speed = speed
+        self.rotationSpeed = rotation_speed
+        self.looking_at = Point(looking_at.x, self.position.y, looking_at.z)
+        self.normal = Vector(0, 1, 0)
+
+
+class Maze:
+    def __init__(self):
+        self.walls = []
+        self.lights = []
+
+    def collision(self, new_pos, offset, matrix):
+        for wall in self.walls:
+            if matrix.is_between(new_pos, wall, offset):
+                return wall
+        return None
+
+
+class Light:
+    def __init__(self, position, diffuse, specular=None):
+        self.position = position
+        self.diffuse = diffuse
+        self.specular = specular if specular else diffuse
+
+
+inputs = {
+            "W": False,  # Walk forward
+            "A": False,  # Turn left
+            "S": False,  # Walk backwards
+            "D": False   # Turn right
+        }
--- mazeControl.py
+++ mazeControl.py
@@ -17,22 +17,16 @@ class Maze3D:
         pygame.init()
         pygame.display.set_mode((800, 600), pygame.OPENGL | pygame.DOUBLEBUF)
 
-        self.game = Game(Shader3D(),
-                         ModelMatrix(),
-                         ViewMatrix(),
-                         ProjectionMatrix(),
-                         Player(Point(0, 3, 10), 10, pi, Point(0, 1, 0))
-                         )
+        self.game = Game(Player(Point(0, 3, 10), 10, pi, Point(0, 1, 0)))
 
         self.game.look()
         self.game.set_perspective(pi/2, 800/600, 0.3, 100)
 
         self.cube = Cube((0.8, 0.3, 0.3), (0.8, 0.3, 0.3), Point(9.0, 3.0, -2.0), (4, 5, 15), 13)
-        self.sphere = Sphere()
-        self.level = Level(Rectangle((0.0, 1.0, 0.0), Point(5.0, 0.0, 5.0), (100.0, 0.1, 100.0)), Maze())
+        self.sphere = Sphere((0.9, 0.4, 0.6), (0.9, 0.5, 0.6), Point(0, 3, 2), (5, 5, 5), 17)
         self.inputs = inputs
 
-        self.level.maze.lights.append(Light(self.game.player.position, (1.0, 1.0, 1.0)))
+        self.game.maze.lights.append(Light(self.game.player.position, (1.0, 1.0, 1.0)))
 
         self.clock = pygame.time.Clock()
         self.clock.tick()
@@ -53,18 +47,18 @@ class Maze3D:
 
         if self.inputs["W"]:
             newpos = self.game.view_matrix.slide(0, 0, -self.game.player.speed * delta_time)
-            if not self.level.collision(newpos, 0.5, self.game.view_matrix):
+            if not self.game.maze.collision(newpos, 0.5, self.game.view_matrix):
                 self.game.view_matrix.eye += newpos
         if self.inputs["S"]:
             newpos = self.game.view_matrix.slide(0, 0, self.game.player.speed * delta_time)
-            if not self.level.collision(newpos, 0.5, self.game.view_matrix):
+            if not self.game.maze.collision(newpos, 0.5, self.game.view_matrix):
                 self.game.view_matrix.eye += newpos
         if self.inputs["A"]:
             self.game.view_matrix.yaw(-self.game.player.rotationSpeed * delta_time)
         if self.inputs["D"]:
             self.game.view_matrix.yaw(self.game.player.rotationSpeed * delta_time)
         self.game.player.position = self.game.view_matrix.eye
-        self.level.maze.lights[0].position = self.game.view_matrix.eye
+        self.game.maze.lights[0].position = self.game.view_matrix.eye
 
     def display(self):
         glEnable(GL_DEPTH_TEST)
@@ -76,46 +70,26 @@ class Maze3D:
 
         self.game.shader.set_view_matrix(self.game.view_matrix.get_matrix())
 
-        self.game.shader.set_light_position(*self.level.maze.lights[0].position)
-        self.game.shader.set_light_color(*self.level.maze.lights[0].diffuse)
+        self.game.shader.set_light_position(*self.game.maze.lights[0].position)
+        self.game.shader.set_light_color(*self.game.maze.lights[0].diffuse)
 
         self.game.model_matrix.load_identity()
 
         # Draw stuff
 
+        # Draw cube
         self.cube.set_vertices(self.game.shader)
-
-        # self.game.shader.set_material_diffuse(0.9, 0.3, 0.3)
-        # self.game.shader.set_material_specular(0.9, 0.7, 0.7)
-        # self.game.shader.set_material_shininess(13)
         self.cube.set_color(self.game.shader)
         self.game.model_matrix.push_matrix()
         self.cube.draw(self.game.model_matrix, self.game.shader)
         self.game.model_matrix.pop_matrix()
-        # for wall in self.level.maze.walls:
-        #     self.game.model_matrix.push_matrix()
-        #     wall.draw(self.game.model_matrix, self.game.shader, self.cube)
-        #     self.game.model_matrix.pop_matrix()
 
-        # # Draw floor
-        # self.shader.set_material_diffuse(1.0, 0.0, 0.0)
-        # self.shader.set_material_specular(0.9, 0.3, 0.2)
-        # self.shader.set_material_shininess(13)
-        # self.model_matrix.push_matrix()
-        # self.model_matrix.add_translation(*self.level.floor.position)
-        # self.model_matrix.add_scale(*self.level.floor.scale)
-        # self.shader.set_model_matrix(self.model_matrix.matrix)
-        # self.cube.draw()
-        # self.model_matrix.pop_matrix()
-        #
-        # self.sphere.set_vertices(self.shader)
-        # self.shader.set_solid_color(0.0, 0.0, 1.0)
-        # self.model_matrix.push_matrix()
-        # self.model_matrix.add_translation(10, 10, 10)
-        # self.model_matrix.add_scale(10, 10, 10)
-        # self.shader.set_model_matrix(self.model_matrix.matrix)
-        # self.sphere.draw()
-        # self.model_matrix.pop_matrix()
+        # Draw sphere
+        self.sphere.set_vertices(self.game.shader)
+        self.sphere.set_color(self.game.shader)
+        self.game.model_matrix.push_matrix()
+        self.sphere.draw(self.game.model_matrix, self.game.shader)
+        self.game.model_matrix.pop_matrix()
 
         pygame.display.flip()
 
